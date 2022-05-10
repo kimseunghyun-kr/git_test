685aa4504b6a9a61307beaf49182e7b36e83d04a
kimseunghyun-kr <incongnito12@gmail.com>
2022-05-10 20:13:16 +0800

    js testing enabled and TOP up to project rock paper scissors

---
+++ js/task.js
@@ -0,0 +1,28 @@
+// task 1
+var admin;
+var names;
+names = 'John';
+admin = names;
+console.log(admin)
+
+// task 2
+const HOME = "EARTH"
+var currVisit;
+
+
+//practice 1
+console.log(23 + 97)
+//practice 3
+console.log((4 + 6 + 9) / 77)
+//practice 4
+let a = 10
+console.log(a);
+9*a
+let b = 7*a
+console.log(b)
+
+//practice 5
+const max = 57
+let actual = max-13
+let percentage = actual/max
+console.log(percentage)
