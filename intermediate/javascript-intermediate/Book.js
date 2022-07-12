b8977f3a20c4958b8b9cbac47061b7d880cd01e8
kimseunghyun-kr <incongnito12@gmail.com>
2022-07-12 23:11:54 +0900

    more library stuff done. need to enable dom for newBook() and add submit. Also need to add function to change state of the books' read status

--- intermediate/javascript-intermediate/Book.js
+++ intermediate/javascript-intermediate/Book.js
@@ -22,10 +22,63 @@ function browseLibrary( libraryArray ) {
     let library = document.querySelector(".libraryTable");
     libraryArray.forEach(x => {
         let temp = document.createElement("div");
+        temp.title = x.title;
         temp.textContent = x.title;
         library.appendChild(temp);
-    })
+        
+        let delBtn = document.createElement('button');
+        delBtn.addEventListener('click', deleteBook);
+        delBtn.textContent = "delete Book";
+        temp.appendChild(delBtn);
+    });
 }
+
+
+
+function newBook() {
+    let entry = document.getElementById("bookEntry");
+    let temp1 = document.createElement("input");
+    temp1.id = 'bookname';
+    temp1.name = 'bookname';
+    let temp2 = document.createElement("input");
+    temp2.id = 'author';
+    temp2.name = 'author';
+    let temp3 = document.createElement("input");
+    temp3.id = 'pages';
+    temp3.name = 'pages';
+    let temp4 = document.createElement('input');
+    temp4.id = 'isRead';
+    temp4.name = "isRead";
+    temp4.type = "checkBox";
+
+    entry.appendChild(temp1);
+    entry.appendChild(temp2);
+    entry.appendChild(temp3);
+    entry.appendChild(temp4);
+
+
+
+    let bookName = document.getElementById("bookName");
+    let author = document.getElementById("author");
+    let pages = document.getElementById("pages");
+    let isRead = document.getElementById("isRead");
+
+    let newBook = new Book(bookName, author, pages, isRead);
+    myLibrary.push(newBook);
+
+}
+
+function deleteBook() {
+    let BookDom = this.parentElement;
+    console.log(BookDom.title);
+    let newLibrary = myLibrary.filter(a => a.title != BookDom.title);
+    console.log(newLibrary);
+    myLibrary = newLibrary;
+    let library = document.querySelector('.libraryTable');
+    library.innerHTML = '';
+    browseLibrary(myLibrary);
+}
+
 
 
 
@@ -36,4 +89,5 @@ Book1.addBookToLibrary();
 Book2.addBookToLibrary();
 
 console.log(myLibrary);
+
 browseLibrary(myLibrary);
